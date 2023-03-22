6e5024ed5af89831963ad2664af35cd01bf97753
pathanimran4373 <pathanimran4373@gmail.com>
2023-03-22 17:33:07 +0530

    mesho site

---
+++ java.js
@@ -0,0 +1,17 @@
+// product Selection//
+// import productData from "./messho/data.js"
+
+// const Category = [...new set(productData.map(el  => el.Category))]
+// console.log(Category)
+
+// const bluetoothEl = document.getElementById("bluetooth")
+// const chainsEl = document.getElementById("chains")
+// const kurtasEl = document.getElementById("kurtas")
+// const accessoriesEl = document.getElementById("accessories")
+// const sareesEl = document.getElementById("sarees")
+// const watchhEl = document.getElementById("watch")
+
+// document.addEventListener(Click)
+
+
+
